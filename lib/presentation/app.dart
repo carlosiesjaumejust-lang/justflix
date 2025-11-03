a6edcc2663695aac1a782e0e405dd521c7b4531d
carlos <carlos@neverlate.es>
2025-11-03 12:15:29 +0100

    estructura clean architecture actualizada

---
+++ lib/presentation/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'screens/home-page.dart';
+
+class JustflixApp extends StatelessWidget {
+  const JustflixApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'JustFlix',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: const HomePage(),
+    );
+  }
+}
